6b59145fe5b6f6b492a13d6ec08b3976bfaa0e45
Anjan Diyora <49095535+Anjan50@users.noreply.github.com>
2020-02-12 16:04:06 +0530

    Update and rename README.md to hello.c

--- hello.c
+++ hello.c
@@ -1,2 +1,7 @@
 # -Hello-World-in-C-262.67-more-points-to-get-your-gold-badge-Rank-18321-Points-237.33-500-C-lan
 Objective  In this challenge, we will learn some basic concepts of C that will get you started with the language. You will need to use the same syntax to read input and write output in many C challenges.
+#include<stdio.h>
+main()
+{
+    printf("Hello, World!\nWelcome to C programming.");
+}
